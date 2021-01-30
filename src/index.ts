7a4aff184bf8729d5d0a8c9e6825c923402d5d90
Jonathan de Kock <jonathan@specno.com>
2021-01-30 12:53:24 +0200

    Abstract scenes | Add mainMenu Scene

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,12 @@
-import Phaser from 'phaser';
-import CharacterImg from './assets/character.png';
-
-class MyGame extends Phaser.Scene
-{
-    private player;
-    private cursors;
-    private velocity:Phaser.Math.Vector2;
-
-    constructor ()
-    {
-        super('demo');
-    }
-
-    preload ()
-    {
-        this.load.image('char', CharacterImg);
-    }
-      
-    create ()
-    {
-        this.player = this.add.image(400, 450, 'char');
-        this.player.scale = 0.8;
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.velocity = new Phaser.Math.Vector2();
-    }
-
-    update()
-    {
-        // move left and right
-        if (this.cursors.left.isDown)
-        {
-            this.velocity.x -= 2;
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.velocity.x += 2;
-        }
-
-        // jump if on ground
-        if (this.cursors.up.isDown && this.player.y >= 450)
-        {
-            this.velocity.y = -30;
-        }
-
-        this.player.x += this.velocity.x;
-        this.player.y += this.velocity.y;
-
-        // gravity
-        if (this.player.y < 450)
-            this.velocity.y += 2;
-        else
-            this.velocity.y = 0;
-
-        // keep character on screen
-        if (this.player.x < 80)
-        {
-            this.player.x = 80;
-            this.velocity.x = 0;
-        }
-        else if (this.player.x > 800 - 80) 
-        {
-            this.player.x = 800 - 80;
-            this.velocity.x = 0;
-        }
-
-        // slow to a stop if no input
-        this.velocity.x *= 0.9;
-    }
-}
+import Phaser from "phaser";
+import { GameScene } from "./scenes/Game";
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+	type: Phaser.AUTO,
+	parent: "phaser-example",
+	width: 800,
+	height: 600,
+	scene: GameScene,
 };
 
 const game = new Phaser.Game(config);
---
+++ src/scenes/Game.ts
@@ -0,0 +1,56 @@
+import CharacterImg from "../assets/character.png";
+
+export class GameScene extends Phaser.Scene {
+	private player;
+	private cursors;
+	private velocity: Phaser.Math.Vector2;
+
+	constructor() {
+		super("demo");
+	}
+
+	preload() {
+		this.load.image("char", CharacterImg);
+	}
+
+	create() {
+		this.player = this.add.image(400, 450, "char");
+		this.player.scale = 0.2;
+
+		this.cursors = this.input.keyboard.createCursorKeys();
+		this.velocity = new Phaser.Math.Vector2();
+	}
+
+	update() {
+		// move left and right
+		if (this.cursors.left.isDown) {
+			this.velocity.x -= 2;
+		} else if (this.cursors.right.isDown) {
+			this.velocity.x += 2;
+		}
+
+		// jump if on ground
+		if (this.cursors.up.isDown && this.player.y >= 450) {
+			this.velocity.y = -30;
+		}
+
+		this.player.x += this.velocity.x;
+		this.player.y += this.velocity.y;
+
+		// gravity
+		if (this.player.y < 450) this.velocity.y += 2;
+		else this.velocity.y = 0;
+
+		// keep character on screen
+		if (this.player.x < 80) {
+			this.player.x = 80;
+			this.velocity.x = 0;
+		} else if (this.player.x > 800 - 80) {
+			this.player.x = 800 - 80;
+			this.velocity.x = 0;
+		}
+
+		// slow to a stop if no input
+		this.velocity.x *= 0.9;
+	}
+}
