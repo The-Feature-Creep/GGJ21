8fd976d19b6fe2e37935ac8d711a6bcda1592563
Jonathan de Kock <jonathan@specno.com>
2021-01-31 15:36:32 +0200

    Change terrain from a sprite to a rectangle for scaling | Change shovel behavior to include sandpile

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import Phaser from 'phaser';
-import { GameScene } from './scenes/Game';
-import { MainMenuScene } from './scenes/MainMenu';
-import { CreditScene } from './scenes/Credits';
+import Phaser from "phaser";
+import { GameScene } from "./scenes/Game";
+import { MainMenuScene } from "./scenes/MainMenu";
+import { CreditScene } from "./scenes/Credits";
 
 export const config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 1000,
-  height: 500,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
-  },
-  backgroundColor: "#633CA6",
-  scene: [MainMenuScene, CreditScene, GameScene],
+	type: Phaser.AUTO,
+	parent: "phaser-example",
+	width: 1000,
+	height: 500,
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: { y: 300 },
+			debug: true,
+		},
+	},
+	backgroundColor: "#633CA6",
+	scene: [MainMenuScene, CreditScene, GameScene],
 };
 
 const game = new Phaser.Game(config);
