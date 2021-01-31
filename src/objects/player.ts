9d087e1d73fbd0c439758e1e97ab74dc7b3b8c79
Dane Brouwer <dane.brouwer@gmail.com>
2021-01-31 10:37:15 +0200

    changes

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -1,71 +1,81 @@
-export const PLAYER_IMG_KEY = "prisoner";
-export const PLAYER_WALK_CYCLE = "prisoner-walk";
-export const PLAYER_STATIONARY_CYCLE = "prisoner-stand";
+// import { PLAYER_WALK_CYCLE } from './player';
+export const PLAYER_IMG_KEY = 'prisoner';
+export const PLAYER_WALK_CYCLE = 'prisoner-walk';
+export const PLAYER_STATIONARY_CYCLE = 'prisoner-stand';
+export const PLAYER_WALK_SHOVEL_CYCLE = 'prisoner-walk-shovel';
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
-	isHidden: boolean = false;
-	hasShovel: boolean = false;
-	shovelActive: boolean = false;
+  isHidden: boolean = false;
+  hasShovel: boolean = false;
+  shovelActive: boolean = false;
 
-	constructor(
-		scene: Phaser.Scene,
-		xPosition: number,
-		yPosition: number,
-		textureURL: string
-	) {
-		super(scene, xPosition, yPosition, textureURL);
-		scene.add.existing(this);
+  constructor(
+    scene: Phaser.Scene,
+    xPosition: number,
+    yPosition: number,
+    textureURL: string
+  ) {
+    super(scene, xPosition, yPosition, textureURL);
+    scene.add.existing(this);
 
-		this.animations(scene);
-		this.physics(scene.physics);
-	}
+    this.animations(scene);
+    this.physics(scene.physics);
+  }
 
-	private animations(scene: Phaser.Scene) {
-		scene.anims.create({
-			key: PLAYER_WALK_CYCLE,
-			frames: scene.anims.generateFrameNumbers(PLAYER_IMG_KEY, {
-				start: 0,
-				end: 7,
-			}),
-			frameRate: 12,
-			repeat: -1,
-		});
-		scene.anims.create({
-			key: PLAYER_STATIONARY_CYCLE,
-			frames: scene.anims.generateFrameNumbers(PLAYER_IMG_KEY, {
-				start: 0,
-				end: 0,
-			}),
-			frameRate: 12,
-			repeat: -1,
-		});
-	}
+  private animations(scene: Phaser.Scene) {
+    scene.anims.create({
+      key: PLAYER_WALK_CYCLE,
+      frames: scene.anims.generateFrameNumbers(PLAYER_IMG_KEY, {
+        start: 0,
+        end: 7,
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+    scene.anims.create({
+      key: PLAYER_STATIONARY_CYCLE,
+      frames: scene.anims.generateFrameNumbers(PLAYER_IMG_KEY, {
+        start: 0,
+        end: 0,
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+    scene.anims.create({
+      key: PLAYER_WALK_SHOVEL_CYCLE,
+      frames: scene.anims.generateFrameNumbers(PLAYER_IMG_KEY, {
+        start: 8,
+        end: 15,
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+  }
+  private physics(physics: Phaser.Physics.Arcade.ArcadePhysics) {
+    physics.add
+      .existing(this)
+      .setSize(100, 110)
+      .setBounce(0.2)
+      .setCollideWorldBounds(true);
+  }
 
-	private physics(physics: Phaser.Physics.Arcade.ArcadePhysics) {
-		physics.add
-			.existing(this)
-			.setSize(100, 110)
-			.setBounce(0.2)
-			.setCollideWorldBounds(true);
-	}
+  get GetHiding() {
+    return this.isHidden;
+  }
+  get GetHasShovel() {
+    return this.hasShovel;
+  }
+  get GetShovelActive() {
+    return this.shovelActive;
+  }
 
-	get GetHiding() {
-		return this.isHidden;
-	}
-	get GetHasShovel() {
-		return this.hasShovel;
-	}
-	get GetShovelActive() {
-		return this.shovelActive;
-	}
-
-	set SetHiding(isHiding: boolean) {
-		this.isHidden = isHiding;
-	}
-	set SetHasShovel(hasS: boolean) {
-		this.hasShovel = hasS;
-	}
-	set SetShovelActive(isHiding: boolean) {
-		this.isHidden = isHiding;
-	}
+  set SetHiding(isHiding: boolean) {
+    this.isHidden = isHiding;
+  }
+  set SetHasShovel(hasS: boolean) {
+    this.hasShovel = hasS;
+  }
+  set SetShovelActive(isHiding: boolean) {
+    this.isHidden = isHiding;
+  }
 }
